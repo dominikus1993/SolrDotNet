5c7e6e30402a8e26aa78ffef3b1433eea82a412d
Dominik.Kotecki <dominikus1910@gmail.com>
2021-12-17 19:28:47 +0100

    add solr collection

--- src/SolrDotNet.Cloud/Solr.fs
+++ src/SolrDotNet.Cloud/Solr.fs
@@ -1,6 +1,13 @@
 namespace SolrDotNet.Cloud
+open System
+open System.Collections.Generic
 
 type internal SolrLiveNode = { Name: string; Url: string }
 
 module internal SolrLiveNode =
     let getAliasUrl (alias: string) (node: SolrLiveNode) = $"{node.Url}/{alias}"
+
+
+type SolrCloudReplica = { IsActive: bool; IsLeader: bool; Name: string; Url: string}
+
+type SolrCloudShard = { IsActive: bool; Name: string; RangeEnd: Nullable<int>; RangeStart: Nullable<int>; Replicas: IReadOnlyDictionary<string, SolrCloudReplica> }
--- src/SolrDotNet.Cloud/Types.fs
+++ src/SolrDotNet.Cloud/Types.fs
@@ -2,6 +2,7 @@ namespace SolrDotNet.Cloud.Types
 open System
 open SolrDotNet.Cloud.Utils
 open SolrDotNet.Cloud
+open System.Collections.Generic
 
 type internal ISolrCollection =
     abstract member GetUrl: unit -> string option
@@ -23,3 +24,22 @@ type SolrAlias(name: string, liveNodes: string array option) =
 
         member this.GetUrl() = urls |> Option.bind(this.getUrl)
 
+type SolrCollection(name: string, shards: IReadOnlyDictionary<string, SolrCloudShard>) =
+    let random = Random()
+    let activeReplicas = shards.Values
+                            |> Seq.filter(fun n -> n.IsActive)
+                            |> Seq.choose(fun x -> x.Replicas |> Option.ofObj)
+                            |> Seq.collect(fun x -> x.Values)
+                            |> Seq.filter(fun x -> x.IsActive)
+                            |> Seq.toArray
+
+    member private _.getUrl(x: SolrCloudReplica array) =
+            match x.Length with
+            | 0 -> failwith "Impossible"
+            | 1 -> Some(x[0])
+            | count -> Some(x[random.Next(0, count)])
+
+    interface ISolrCollection with
+        member _.Name with get() = name
+
+        member this.GetUrl() = this.getUrl(activeReplicas) |> Option.map(fun x -> x.Url)
