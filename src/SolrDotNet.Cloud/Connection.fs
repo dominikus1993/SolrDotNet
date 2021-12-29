aed82677f4f4a5a94657be2779e2a7dc9cb14f08
Dominik.Kotecki <dominikus1910@gmail.com>
2021-12-29 17:49:33 +0100

    add getaliases

--- src/SolrDotNet.Cloud/Connection.fs
+++ src/SolrDotNet.Cloud/Connection.fs
@@ -16,3 +16,17 @@ type ISolrCloudConnection =
     abstract member Key: string with get
     abstract member Init: unit -> Task
 
+module internal SolrAlias =
+    open org.apache.zookeeper
+    open SolrDotNet.Cloud.Utils
+    open Newtonsoft.Json
+    open System.Text
+
+    let loadAliases (z: ZooKeeper)(root: string) =
+        task {
+            let! aliasesJson = z.getDataAsync(Zookeeper.getAliasesUrlPath(root), true)
+            if isNull aliasesJson.Data then
+                return None
+            else
+                return Some(JsonConvert.DeserializeObject<SolrAliases>(Encoding.UTF8.GetString(aliasesJson.Data)))
+        }
--- src/SolrDotNet.Cloud/Solr.fs
+++ src/SolrDotNet.Cloud/Solr.fs
@@ -1,6 +1,7 @@
 namespace SolrDotNet.Cloud
 open System
 open System.Collections.Generic
+open Newtonsoft.Json
 
 type internal SolrLiveNode = { Name: string; Url: string }
 
@@ -11,3 +12,9 @@ module internal SolrLiveNode =
 type SolrCloudReplica = { IsActive: bool; IsLeader: bool; Name: string; Url: string}
 
 type SolrCloudShard = { IsActive: bool; Name: string; RangeEnd: Nullable<int>; RangeStart: Nullable<int>; Replicas: IReadOnlyDictionary<string, SolrCloudReplica> }
+
+[<CLIMutable>]
+type SolrAliases = { [<JsonProperty("collection")>]Collection: Dictionary<string, string> }
+
+module SolrAliases =
+    let isEmpty (a: SolrAliases) = a.Collection.Count = 0
