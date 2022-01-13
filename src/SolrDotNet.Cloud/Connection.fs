32e7c4f6568c6c971a64b2242ca3a2831b4c5e35
Dominik.Kotecki <dominikus1910@gmail.com>
2022-01-13 18:05:24 +0100

    add LiveNodes load method

--- src/SolrDotNet.Cloud/Connection.fs
+++ src/SolrDotNet.Cloud/Connection.fs
@@ -3,6 +3,8 @@
 open SolrDotNet.Cloud.Types
 open System
 open System.Threading.Tasks
+open org.apache.zookeeper
+open SolrDotNet.Cloud.Utils
 
 type ISolrCloudState =
     interface
@@ -19,23 +21,47 @@ type ISolrCloudConnection =
     abstract member Key : string
     abstract member Init : unit -> Task
 
+[<AbstractClass>]
+type ZookeeperSolrCloudConnection() =
+    inherit Watcher()
+    abstract member Zookeeper : ZooKeeper
+
 module internal SolrAlias =
     open org.apache.zookeeper
     open SolrDotNet.Cloud.Utils
     open Newtonsoft.Json
     open System.Text
 
-    let loadAliases (z: ZooKeeper) (root: string) =
+    let loadAliases (z: ZookeeperSolrCloudConnection) (root: string) =
         task {
-            let! aliasesJson = z.getDataAsync (Zookeeper.getAliasesUrlPath (root), true)
+            let! aliasesJson = z.Zookeeper.getDataAsync (Zookeeper.getAliasesUrlPath (root), true)
 
             return
                 aliasesJson
                 |> Option.ofObj
                 |> Option.bind (fun json -> json.Data |> Option.ofObj)
-                |> Option.map(fun data -> JsonConvert.DeserializeObject<SolrAliases>(Encoding.UTF8.GetString(data)))
+                |> Option.map (fun data -> JsonConvert.DeserializeObject<SolrAliases>(Encoding.UTF8.GetString(data)))
         }
 
+module internal LiveNodes =
+    open org.apache.zookeeper
+    open SolrDotNet.Cloud.Utils
+    open Newtonsoft.Json
+    open System.Text
+    open SolrDotNet.Cloud.Utils
+
+    let load (z: ZookeeperSolrCloudConnection) (root: string) =
+        task {
+            let! liveNodesChildren = z.Zookeeper.getChildrenAsync (Zookeeper.getLiveNodesUrlPath (root), z)
+
+            let children =
+                liveNodesChildren.Children
+                |> Option.ofObj
+                |> Option.filter (fun x -> x.Count > 0)
+                |> Option.map (fun x -> x.ToArray())
+
+            return SolrLiveNodesParser.parse (children)
+        }
 
 type ZooKeeperSolrCloudConnection() =
     class
