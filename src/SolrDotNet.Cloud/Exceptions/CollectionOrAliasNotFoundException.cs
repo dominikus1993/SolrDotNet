71eff7b110e2e1751b548943641d7078028add9e
Dominik.Kotecki <dominik.kotecki@rossmann.com.pl>
2022-03-09 09:26:06 +0000

    add CollectionOrAliasNotFoundException

---
+++ src/SolrDotNet.Cloud/Exceptions/CollectionOrAliasNotFoundException.cs
@@ -0,0 +1,22 @@
+namespace SolrDotNet.Cloud.Exceptions;
+
+public class CollectionOrAliasNotFoundException : Exception
+{
+    public string CollectionName { get; }
+    public CollectionOrAliasNotFoundException(string collectionName) : base()
+    {
+        CollectionName = collectionName;
+    }
+
+    public CollectionOrAliasNotFoundException(string? message, string collectionName) : base(message)
+    {
+        CollectionName = collectionName;
+    }
+
+    public CollectionOrAliasNotFoundException(string? message, Exception? innerException, string collectionName) : base(message, innerException) { CollectionName = collectionName; }
+
+    public override string ToString()
+    {
+        return $"Collection or alias not found: {CollectionName}, {base.ToString()}";
+    }
+}
